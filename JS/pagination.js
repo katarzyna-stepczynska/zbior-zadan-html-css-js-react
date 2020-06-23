26899aa3e7eec1c886517c40ff43055b12814440
Katarzyna <stepczynska.katarzyna@gmail.com>
2020-06-23 18:25:49 +0200

    Fill in function of pagination. Not finished yet. Troubles with keys or parameter of function

--- JS/pagination.js
+++ JS/pagination.js
@@ -1,8 +1,45 @@
 // Stwórz funkcję paginateArray
 const paginateArray = (dataEntries, settings) => {
-    // ...
-    // return entriesOnSelectedPage
-}
+  // dataEntries = 10; // zakres stron do wyswietlenia? czy powinno byc w array?
+  // settings = { // wyswietla mi bład w konsoli
+  //   actualPageIdx, // aktualna strona
+  //   entriesOnPage // max ilość stron
+  // };
+  let entriesOnSelectedPage; // ilosc elementów do wyswietlenia
+  let startPage; // początek strony wyswietlanej
+  let endPage; // koniec strony wyswietlanej
+
+  if (settings.actualPageIdx < 1) {
+    settings.actualPageIdx = 1;
+  } else if (settings.actualPageIdx > settings.entriesOnPage) {
+    settings.actualPageIdx = settings.entriesOnPage;
+  }
+  if (settings.entriesOnPage <= dataEntries) {
+    startPage = 1;
+    endPage = settings.entriesOnPage;
+  } else {
+    let beforePage = Math.floor(dataEntries / 2);
+    let afterPage = Math.ceil(dataEntries / 2) - 1;
+    if (settings.actualPageIdx <= beforePage) {
+      startPage = 1;
+      endPage = dataEntries;
+    } else if (settings.actualPageIdx + afterPage >= settings.entriesOnPage) {
+      startPage = settings.entriesOnPage - dataEntries + 1;
+      endPage = settings.entriesOnPage;
+    } else {
+      startPage = settings.actualPageIdx - beforePage;
+      endPage = settings.actualPageIdx + afterPage;
+    }
+  }
+  entriesOnSelectedPage = Array.from(endPage - startPage); // nie wiem jak sprawdzic czy działa bo mam problem z settings
+  return entriesOnSelectedPage;
+};
+const settingsNow = {
+  // tu mam problem bo nie wiem jak inaczej wywołac dwa klucze dla jednego parametru funkcji
+  actualPageIdx: 9,
+  entriesOnPage: 50,
+};
+paginateArray(10, settingsNow);
 
 // która przyjmuję array do paginacji dataEntries oraz settings o kluczach { actualPageIdx=9, entriesOnPage=50 }
 // - actualPageIdx to index wybranej strony
